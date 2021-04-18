e577b4e6ff6b19e2dbcdf35164f7f2fde7ad0fd1
LinHQ1999 <1157136867@qq.com>
2021-04-18 18:54:09 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
 mkdir dist
 ng build --base-href /app/statics/ --prod
-cd backend
+Set-Location backend
+Write-Output
 go build -o fuckcamp.exe
-mv fuckcamp.exe ../dist
-rm -force statics
-cd ..
+Move-Item fuckcamp.exe ../dist
+Copy-Item users.json ../dist
+Remove-Item -force statics/*
+Set-Location ..
