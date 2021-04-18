4039a9df87b25e0ede2977711b6025d866cb3ecf
LinHQ1999 <1157136867@qq.com>
2021-04-18 18:31:49 +0800

    更小间距

--- src/app/studata.service.ts
+++ src/app/studata.service.ts
@@ -1,13 +1,14 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {Student} from './Student'
 
 @Injectable({
     providedIn: 'root'
 })
 export class StudataService {
-    userListUrl: string = "/linhq/users";
+    // userListUrl: string = "/linhq/users";
+    userListUrl: string = "assets/users.json";
     constructor(private client: HttpClient) {}
 
     getStudents(): Observable<Student[]> {
